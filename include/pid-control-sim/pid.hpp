b14d3dd1f223d766b96c7be7283ab316d20db912
atharv <atharvghone@gmail.com>
2026-07-17 18:58:07 -0400

    in progress implementation

---
+++ include/pid-control-sim/pid.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdint>
+
+class PIDControl {
+public:
+  PIDControl();
+
+  auto compute(uint64_t setpoint, uint64_t measured_value, uint64_t dt)
+      -> int64_t;
+
+private:
+  int64_t k_p_;
+  int64_t k_i_;
+  int64_t k_d_;
+
+  int64_t integral_;
+  int64_t integral_min_;
+  int64_t integral_max_;
+
+  int64_t output_min_;
+  int64_t output_max_;
+
+  static auto clamp(int64_t value, int64_t min_value, int64_t max_value)
+      -> int64_t;
+
+  auto compute_proportional_term(int64_t error) -> int64_t;
+
+  auto compute_integral_term() -> int64_t;
+
+  auto compute_derivate_term() -> int64_t;
+};
